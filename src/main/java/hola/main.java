2d5d6f2908baf693cf273a51850a5fb4885645a2
FacundoSpreafico <fspreafico@frsf.utn.edu.ar>
2023-11-11 16:34:09 -0300

    commitRama

--- src/main/java/hola/main.java
+++ src/main/java/hola/main.java
@@ -3,10 +3,12 @@ package hola;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.print("Hola esta es la version numero 2");
-		
+		int a=0,b=2,c=6;
+		sumar(a,b,c);
 	}
 	
-	
+	public static void sumar(int a,int b, int c) {
+		System.out.print(a+b+c);
+	}
 
 }
