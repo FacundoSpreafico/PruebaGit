67f1919df3b882b91170bd9ffe2a7d8ec0ea0e5c
FacundoSpreafico <fspreafico@frsf.utn.edu.ar>
2023-11-11 16:48:22 -0300

    actualizacion

--- src/main/java/hola/main.java
+++ src/main/java/hola/main.java
@@ -1,10 +1,11 @@
 package hola;
 
 public class main {
-
+	
 	public static void main(String[] args) {
 		int a=0,b=2,c=6;
 		sumar(a,b,c);
+		
 	}
 	
 	public static void sumar(int a,int b, int c) {
