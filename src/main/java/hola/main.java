c9de9a3e5db8e470816b904eab7359612ce9263c
FacundoSpreafico <fspreafico@frsf.utn.edu.ar>
2023-11-11 16:51:14 -0300

    actualizacionResta

--- src/main/java/hola/main.java
+++ src/main/java/hola/main.java
@@ -5,9 +5,13 @@ public class main {
 	public static void main(String[] args) {
 		int a=0,b=2,c=6;
 		sumar(a,b,c);
-		
+		restar(a,b);
 	}
 	
+	public static void restar(int a,int b) {
+		System.out.print(a-b);
+		
+	}
 	public static void sumar(int a,int b, int c) {
 		System.out.print(a+b+c);
 	}
